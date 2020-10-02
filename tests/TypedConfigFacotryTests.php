e6c8613729f81c16c679396aa977d99ca086fc98
Jeffrey Becker <jeffrey.becker@projekt202.com>
2020-10-02 15:30:17 -0500

    class creation tests

--- tests/TypedConfigFacotryTests.php
+++ tests/TypedConfigFacotryTests.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ferb\ConfTests;
 
 use Ferb\Conf\Util\TypedConfigFactory;
+use Ferb\Conf\ConfigBuilder;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -23,6 +24,11 @@ class TypedConfigFactoryTests extends TestCase
             ]
             ];
     }
+    private function integration_data(){
+        return  [
+            'example'=> $this->data()
+        ];
+    }
     public function testCanCreateWithoutConstructor(){
         $result = TypedConfigFactory::instantiate(Outer::class, $this->data());
         $this->assertNotEmpty($result);
@@ -35,17 +41,37 @@ class TypedConfigFactoryTests extends TestCase
         $this->assertEquals(1, $result->a);
         $this->assertEquals(5, $result->c->e);
     }
+
+    public function canIntegrateWithConstructor(){
+        $conf = (new ConfigBuilder())
+            ->add_array($this->integration_data())
+            ->create();
+        $result = $conf->section('example')->as(OuterConstruct::class);
+        $this->assertNotEmpty($result);
+        $this->assertEquals(1, $result->a);
+        $this->assertEquals(5, $result->c->e);
+    }
+
+    public function canIntegrateWithoutConstructor(){
+        $conf = (new ConfigBuilder())
+            ->add_array($this->integration_data())
+            ->create();
+        $result = $conf->section('example')->as(Outer::class);
+        $this->assertNotEmpty($result);
+        $this->assertEquals(1, $result->a);
+        $this->assertEquals(5, $result->c->e);
+    }
 }
 
 
 class OuterConstruct{
     public int $a;
     public string $b;
-    public Inner $c;
+    public InnerConstruct $c;
     public function __construct($arr){
             $this->a = $arr['a'];
             $this->b = $arr['b'];
-            $this->c = new Inner($arr['c']);
+            $this->c = new InnerConstruct($arr['c']);
 
     }
 }
